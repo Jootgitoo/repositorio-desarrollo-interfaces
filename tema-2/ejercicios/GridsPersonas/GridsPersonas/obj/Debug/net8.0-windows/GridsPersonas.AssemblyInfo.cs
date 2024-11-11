b674cf27e69b37b4bb012a8e288989264ec5eb8d
JorgeDam26 <165775297+JorgeDam26@users.noreply.github.com>
2024-11-11 12:54:45 +0100

    3 frames

--- tema-2/ejercicios/GridsPersonas/GridsPersonas/obj/Debug/net8.0-windows/GridsPersonas.AssemblyInfo.cs
+++ tema-2/ejercicios/GridsPersonas/GridsPersonas/obj/Debug/net8.0-windows/GridsPersonas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GridsPersonas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34dc385c970d4cfc71a30b4196b6208fbf745d1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f1951c4384c160500e3e906d05a45fd6a7fa9bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("GridsPersonas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GridsPersonas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
