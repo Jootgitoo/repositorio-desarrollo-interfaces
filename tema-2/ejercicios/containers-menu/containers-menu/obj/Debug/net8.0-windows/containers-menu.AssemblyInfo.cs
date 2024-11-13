d9a7daa146de17f7d4348673203cb1a686ba2704
JorgeDam26 <jorgehm260505@gmail.com>
2024-11-13 10:13:40 +0100

    a

--- tema-2/ejercicios/containers-menu/containers-menu/obj/Debug/net8.0-windows/containers-menu.AssemblyInfo.cs
+++ tema-2/ejercicios/containers-menu/containers-menu/obj/Debug/net8.0-windows/containers-menu.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("containers-menu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34dc385c970d4cfc71a30b4196b6208fbf745d1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b674cf27e69b37b4bb012a8e288989264ec5eb8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("containers-menu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("containers-menu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
