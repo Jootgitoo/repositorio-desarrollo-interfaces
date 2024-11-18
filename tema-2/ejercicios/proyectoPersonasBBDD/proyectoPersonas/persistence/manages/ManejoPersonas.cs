f99f7c5edc4ad57c1369daab39955229a29652bc
Jorge <jorgehm260505@gmail.com>
2024-11-18 22:30:56 +0100

    personas hecho

---
+++ tema-2/ejercicios/proyectoPersonasBBDD/proyectoPersonas/persistence/manages/ManejoPersonas.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataGridPersonas.persistence;
+using proyectoPersonas.domain;
+
+namespace proyectoPersonas.persistence.manages
+{
+  internal class ManejoPersonas
+  {
+        // constantes & variables =>
+        private DataTable dataTable;
+        private List<Persona> listaPersonas;
+        int lastId;
+
+     //————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+     // constructores =>
+        public ManejoPersonas()
+        {
+              dataTable = new DataTable();
+              listaPersonas = new List<Persona>();
+        }
+
+    //————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+    // metodos publicos =>
+        // metodo | leerPersonas | lee todas las personas de la base de datos y las almacena en una lista que devuelve ==>
+        public List<Persona> leerPersonas ()
+        {
+            List<Object> listaAux = DBBroker.obtenerAgente().leer("SELECT * FROM people1.people;");
+            foreach (List<Object> aux in listaAux)
+            {
+                Persona persona = new Persona();
+                Console.WriteLine(aux.ToString());
+                persona = new Persona(Convert.ToInt32(aux[0]), aux[1].ToString(), Convert.ToInt32(aux[2]));
+                listaPersonas.Add(persona);
+            }
+
+            return listaPersonas;
+        }
+
+        // metodo | genListaPersonas | genera y devuelve una lista de personas ==>
+        public void insertarPersona(Persona inputPersona)
+        {
+            DBBroker dbBroker = DBBroker.obtenerAgente();
+            dbBroker.modificar("Insert into people1.people (id,name,age) values ("+inputPersona.Id+",'"+inputPersona.Nombre+"', "+inputPersona.Edad + ");");
+        }
+
+        // metodo | getLastId | obtiene el ultimo id y le agrega 1 ==>
+        public int getLastId (Persona inputPersona)
+        {
+              List<Object> listaAux;
+              listaAux = DBBroker.obtenerAgente().leer("select COALESCE(MAX(id), 0) FROM people1.people");
+
+              foreach (List<Object > aux in listaAux)
+              {
+                    lastId = Convert.ToInt32(aux[0]) + 1;
+              }
+
+              return lastId;
+        }
+
+      // metodo | deletePersona | elimina una persona ==>
+        public void deletePersona (Persona inputPersona)
+        {
+            DBBroker dBBroker = DBBroker.obtenerAgente();
+            dBBroker.modificar("DELETE FROM people1.people WHERE id = " + inputPersona.Id + ";");
+        }
+
+        // metodo | modificarPersona | modifica una persona ==>
+        public void modificarPersona (Persona inputPersona)
+        {
+            DBBroker dbBroker = DBBroker.obtenerAgente();
+            dbBroker.modificar("UPDATE people1.people SET name = '" + inputPersona.Nombre + "', age = " + inputPersona.Edad.ToString() + " WHERE id = " + inputPersona.Id + ";");
+        }
+    }
+}
