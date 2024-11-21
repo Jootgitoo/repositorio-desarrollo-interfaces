6ea0d02460dbc6ec00b45cd56fba7e3cdce14568
JorgeDam26 <jorgehm260505@gmail.com>
2024-11-21 11:25:14 +0100

    ejercicios manejo excepciones

---
+++ tema-2/ejercicios/ManejoExcepciones/ManejoExcepciones/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManejoExcepciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Divisiones();
+        }
+
+
+        public static void Divisiones()
+        {
+            int[] arrayResultados = new int[10];
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("Indicanos un numero: ");
+                    int num1 = Int32.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Indicanos otro numero: ");
+                    int num2 = Int32.Parse(Console.ReadLine());
+
+                    int resultado = num1 / num2;
+
+                    arrayResultados[i] = resultado;
+
+                }
+
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("No se puede dividir entre 0");
+            }
+            catch (IndexOutOfRangeException ex) 
+            {
+                Console.WriteLine("El array es más pequeña del valor seleccionado");
+            }
+            finally
+            {
+                Console.WriteLine("Programa finalizado");
+            }
+
+        }    
+    }
+}
