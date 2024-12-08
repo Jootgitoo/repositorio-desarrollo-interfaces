1bf0b52015e71307bcdcad868e4094279fccc85e
Jorge <jorgehm260505@gmail.com>
2024-12-08 21:59:20 +0100

    avance tpv

--- tema-2/ejercicios/ejercicio-tpv/tpvCamisetasFutbol/tpvCamisetasFutbol/view/MainWindow.xaml.cs
+++ tema-2/ejercicios/ejercicio-tpv/tpvCamisetasFutbol/tpvCamisetasFutbol/view/MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace tpvCamisetasFutbol
         {
             InitializeComponent();
 
-            Producto producto = new Producto(1, 30.00, "Primera quipacion FCB 24-25", 1);
+            //Producto producto = new Producto(1, 30.00, "Primera quipacion FCB 24-25", 1);
             Producto producto1 = new Producto(2, 25.50, "Primera equipacion RM 24-25", 1);
             Producto producto2 = new Producto(3, 70.99, "Edicion limitada RM", 1);
             Producto producto3 = new Producto(4, 150.99, "Edicion limitada FCB", 1);
@@ -35,7 +35,7 @@ namespace tpvCamisetasFutbol
             Producto producto7 = new Producto(8, 30.00, "Primera equpacion Betis 24-25", 1);
 
 
-            producto.insertar();
+           //producto.insertar();
             producto1.insertar();
             producto2.insertar();   
             producto3.insertar();
